b80c534c1db6e33788730ae980b3d5e95822c50e
Ryan Haver <ryanhaver168@gmail.com>
2023-11-21 12:02:53 -0500

    Parsing table data done

--- ForexNewsScraper.py
+++ ForexNewsScraper.py
@@ -2,24 +2,40 @@ from selenium.webdriver.support.ui import Select
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.options import Options
+#from selenium.webdriver.edge.options import Options
+from selenium.webdriver.safari.options import Options
+from time import sleep
 
 # Source: https://www.investing.com/economic-calendar/
 edge_options = Options()
 edge_options.add_argument("headless")
 
-driver = webdriver.Edge(options=edge_options)
+driver = webdriver.Safari(options=edge_options)
 driver.get("https://www.investing.com/economic-calendar/")
 
-table = driver.find_elements(By.ID, "economicCalendarData")
-#print(table.text)
-#table = table[1:]
-print("Table is found")
-print(table[0].text)
-#for row in table:
-#    print(row.text)
+this_week_btn = driver.find_element(By.ID, "timeFrame_thisWeek")
+this_week_btn.click() # Naviate to this week
 
-#print(table.find_element(By.ID, "theDay1699747200"))
-#print(driver.find_element(By.CLASS_NAME, "calendar__print calendar__print--header"))
+sleep(3) # Wait for page to get updated
+
+table = driver.find_elements(By.ID, "economicCalendarData")
+table_elem = table[0]
+rows = table_elem.find_elements(By.TAG_NAME, "tr")
+
+rows = rows[2:]
+
+for row in rows:
+    row_data = row.find_elements(By.TAG_NAME, "td")
+
+    if (len(row_data) > 2): #Will not trigger on headings
+        time = row_data[0].text.strip()
+        sentiment_text = row_data[2].get_attribute("title")
+        event_name = row_data[3].text.strip()
+        print("Event: " + event_name + " at: " + time + " vol: " + sentiment_text)
+    else:
+        new_day = row_data[0].text.strip()
+        print("NEW DAY: " + new_day)
+
+
 
 driver.quit()
