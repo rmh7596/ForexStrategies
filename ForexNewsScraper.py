e091a0ddd83c07ec806675c0d549bda677144bc7
Ryan Haver <ryanhaver168@gmail.com>
2023-11-30 22:46:53 -0500

    Events are grouped by day now

--- ForexNewsScraper.py
+++ ForexNewsScraper.py
@@ -17,12 +17,18 @@ this_week_btn = driver.find_element(By.ID, "timeFrame_thisWeek")
 this_week_btn.click() # Naviate to this week
 
 sleep(3) # Wait for page to get updated
+for i in range(30): #Ensure we hit the bottom of the page
+    sleep(0.25)
+    driver.execute_script("window.scrollBy(0, window.innerHeight)", "")
+
 
 table = driver.find_elements(By.ID, "economicCalendarData")
 table_elem = table[0]
 rows = table_elem.find_elements(By.TAG_NAME, "tr")
 
 rows = rows[2:]
+day_lst = []
+days = []
 
 for row in rows:
     row_data = row.find_elements(By.TAG_NAME, "td")
@@ -30,12 +36,18 @@ for row in rows:
     if (len(row_data) > 2): #Will not trigger on headings
         time = row_data[0].text.strip()
         sentiment_text = row_data[2].get_attribute("title")
+        country = row_data[1].text.strip()
         event_name = row_data[3].text.strip()
-        print("Event: " + event_name + " at: " + time + " vol: " + sentiment_text)
+        day_lst.append((time,sentiment_text,country))
     else:
         new_day = row_data[0].text.strip()
-        print("NEW DAY: " + new_day)
+        days.append(day_lst) # Append the old list
+        day_lst = []
+        day_lst.append(new_day) # Start the new one
 
-
+days.append(day_lst) # To ensure Saturday is included
 
 driver.quit()
+
+del days[0] # Remove the first list
+print(days)
