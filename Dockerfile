c1efc1592fe386938eafcf9ab2a162e7f230ada4
Ryan Haver <rmh7596@rit.edu>
2024-04-22 22:09:18 -0400

    Removed port info

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,6 @@ WORKDIR /app
 RUN pip install -r requirements.txt
 
 COPY . /app
-EXPOSE 5000
 
 ENTRYPOINT [ "python" ]
 
--- ForexNewsScraper.py
+++ ForexNewsScraper.py
@@ -97,4 +97,4 @@ if __name__ == "__main__":
 
     scheduler.start()
     
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", debug=True)
